a24ab26fe69752c9a879b7051f27b9da0c6d13cd
Rohan <ronuchit@yahoo.com>
2015-06-13 13:50:34 -0700

    day 3 results

--- plots.py
+++ plots.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 days = [3, 4, 5]
 orig = [0.2577, 0.2577, 0.2577]
 repeat_common = [0.2335, 0.2493, 0.2704]
-repeat_symbol = [0.2, 0.2, 0.2]
-no_repeats = [0.3, 0.3, 0.3]
+repeat_symbol = [0.2644, 0.2, 0.2]
+no_repeats = [0.2404, 0.3, 0.3]
 
 plt.plot(days, orig, "k--", days, repeat_common, "r", days, repeat_symbol, "g", days, no_repeats, "b")
 plt.axis([3, 5, 0.2, 0.28])
